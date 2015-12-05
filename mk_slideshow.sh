6fa2cc3b93c6d4c43fa4531a836d53a5b96d5f27
mikeplem <mikeplemmons@gmail.com>
2015-12-05 16:52:35 -0500

    initial slideshow checkin

---
+++ mk_slideshow.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# the idea behind this slide show comes from
+# https://css-tricks.com/snippets/jquery/simple-auto-playing-slideshow/
+
+cat <<-ENDOFHEADER
+<html>
+<head>
+<title>CrossChx Employees</title>
+<script src='jquery-2.1.4.min.js'></script>
+<script src='slideshow.js'></script>
+<link rel='stylesheet' href='slideshow.css'>
+</head>
+<body>
+<div class='slideshow'>
+ENDOFHEADER
+
+for file in $(ls *.jpg *.JPG 2>/dev/null); do
+    echo "<div><img src='${file}'></div>"
+done
+
+cat <<-ENDOFFOOTER
+</div>
+</body>
+</html>
+ENDOFFOOTER
